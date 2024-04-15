fb99b13094ac660b2d20626fd74094adc939185c
Steve Eick <steve.eick@gmail.com>
2024-04-15 14:12:06 -0600

    Initial version from https://spring.io/guides/tutorials/spring-boot-kotlin

---
+++ src/main/kotlin/com/example/fsma_schema/FsmaSchemaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.fsma_schema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FsmaSchemaApplication
+
+fun main(args: Array<String>) {
+	runApplication<FsmaSchemaApplication>(*args)
+}
