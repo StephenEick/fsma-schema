d29f49e38eaec6cad64041e8f657e44c2ec18aa5
Steve Eick <steve.eick@gmail.com>
2024-05-21 09:01:05 -0600

    Remove original code

--- src/main/kotlin/com/example/fsma/repository/old/ArticleRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fsma.repository.old
-
-import com.example.fsma.model.old.Article
-import org.springframework.data.repository.CrudRepository
-
-interface ArticleRepository : CrudRepository<Article, Long> {
-    fun findBySlug(slug: String): Article?
-    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
-}
--- src/main/kotlin/com/example/fsma/repository/old/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fsma.repository.old
-
-import com.example.fsma.model.FsmaUser
-import org.springframework.data.repository.CrudRepository
-
-interface UserRepository : CrudRepository<FsmaUser, Long> {
-    fun findByLogin(login: String): FsmaUser?
-}
